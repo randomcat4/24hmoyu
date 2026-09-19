a77083c24f1f6cecaf86ed904e83dcd5e4878366
randomcat4 <273058269+randomcat4@users.noreply.github.com>
2026-09-19 20:31:58 +0800

    feat(memory): export memory API

---
+++ src/twentyfour_myself/memory/__init__.py
@@ -0,0 +1,15 @@
+from .dream import DreamEngine, DreamModel, DreamResult, OpenAICompatibleDreamModel
+from .models import MemoryAction, MemoryItem, MemoryKind, MemoryOperation
+from .store import MemoryStore
+
+__all__ = [
+    "DreamEngine",
+    "DreamModel",
+    "DreamResult",
+    "OpenAICompatibleDreamModel",
+    "MemoryAction",
+    "MemoryItem",
+    "MemoryKind",
+    "MemoryOperation",
+    "MemoryStore",
+]
